3c016646be9667d938a4bd6947c110ee4ea89ca4
Michelle Shirk <michelle.l.shirk@gmail.com>
2023-03-14 11:14:01 -0400

    first commit

---
+++ insertion.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+    for (let i = 0; i < arr.length; i++){
+        let current = arr[i];
+
+        for (var j = i - 1; j > - 1 && arr[j] > current; j--){
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+
+module.exports = insertionSort;
